f1e98b5cbf51e6ddc11bde6c07caeff11a9a1eaa
jacerniello <75341926+jacerniello@users.noreply.github.com>
2025-03-06 10:45:34 -0500

    made first prototype

--- schedulingsolar/settings.py
+++ schedulingsolar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j77ihr15#je!i^)#*7yfz!o842n&)j-50p(&f%+dep0*geq^0h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "172.25.152.32"]
 
 
 # Application definition
